a3f33566a7ac90fc3cfa04ed7465c2254594eae3
Agata <agata.react@gmail.com>
2022-11-19 13:19:25 +0100

    Room Details section updated and RoomDetials styles

--- src/views/RoomDetails.js
+++ src/views/RoomDetails.js
@@ -1,6 +1,10 @@
+import { Button } from "../common/Button";
+import { RoomsList } from "./RoomsList";
+
 export function RoomDetails(id) {
 
   const section = document.createElement('section');
+  section.classList.add('roomDetails');
 
   section.innerHTML = `
       <header>Loading...</header>
@@ -9,9 +13,20 @@ export function RoomDetails(id) {
   fetch(`https://my-json-server.typicode.com/luapax/jsonServer/rooms/${id}`)
     .then(response => response.json())
     .then(room => {
+
+      const backButton = Button('Back to Room List', () => {
+        const navigateEvent = new CustomEvent('navigate', {
+          detail: () => RoomsList()
+        });
+
+        document.body.dispatchEvent(navigateEvent);
+      });
+
       const paragraph = document.createElement('p');
 
       paragraph.innerHTML = `
+          <img src=${room.img} width=auto height=150></img>
+          <div>
           <strong>Nazwa:</strong> ${room.name}
           <br/>
           <strong>Beds:</strong> ${room.beds}x🛏️
@@ -21,10 +36,16 @@ export function RoomDetails(id) {
           <strong>Description:</strong> ${room.description}
           <br/>
           <strong>Price:</strong> ${room.price.toFixed(2)} zł
+          </div>
         `;
+
+
 
       section.querySelector('header').remove();
       section.append(paragraph);
+
+
+      section.append(backButton);
     });
 
   return section;
